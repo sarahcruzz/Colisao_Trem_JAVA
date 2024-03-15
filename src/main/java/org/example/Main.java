316d78cd986ced150abf991699c9be398cae9db9
sarahcruzz <sarah.cruz0492@gmail.com>
2024-03-15 11:22:22 -0300

    colisao de trem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // declaração de variáveis
-        double posicaoA, posicaoB, velocidadeA, velocidadeB, tempo, posColisao, posFinalA, posFinalB;
+        double posicaoA, posicaoB, velocidadeA, velocidadeB, tempo, posFinalA, posFinalB;
+        int confirma;
 
         // criação do objeto da classe Scanner
         Scanner scanner = new Scanner(System.in);
@@ -13,6 +14,7 @@ public class Main {
         // entradas
 
         do {
+            System.out.println("A posição do trem deve estar entre 0KM a 10.000KM");
             System.out.print("Digite a posição inicial do trem A: ");
             posicaoA = scanner.nextDouble();
             System.out.print("Digite a posição inicial do trem B: ");
@@ -21,18 +23,20 @@ public class Main {
         } while ((posicaoA < 0 || posicaoA > 10000) && (posicaoB < 0 || posicaoB > 10000));
 
 
+        do {
+            System.out.print("Digite a velocidade do trem A: ");
+            velocidadeA = scanner.nextDouble();
+            System.out.print("Digite a velocidade do trem B: ");
+            velocidadeB = scanner.nextDouble();
+        } while ((velocidadeA >= 0 || velocidadeA <= 300 ) && (velocidadeB <= 0 || velocidadeB <= -300 ));
 
-        System.out.print("Digite a velocidade do trem A: ");
-        velocidadeA = scanner.nextDouble();
-
-        System.out.print("Digite a velocidade do trem B: ");
-        velocidadeB = scanner.nextDouble();
 
         // processamento
         tempo = (posicaoA - posicaoB) / (velocidadeB - velocidadeA);
         posFinalA = posicaoA + (velocidadeA * tempo);
-        posFinalB = posicaoB + (velocidadeB * tempo);
+        posFinalB = posicaoB - (velocidadeB * tempo);
 
         // saida
+
     }
 }
